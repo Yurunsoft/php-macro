b705f69cd1226a02a12187265a556cbd1e935156
Yurun <admin@yurunsoft.com>
2021-11-27 10:52:08 +0800

    优化代码

--- src/Parser/IfParser.php
+++ src/Parser/IfParser.php
@@ -11,10 +11,10 @@ class IfParser implements IMacroParser
     public function parse(string $content): string
     {
         $resultContent = $content;
-        $resultContent = preg_replace('/^#if\s+(.+)$/m', '<?php if ($1): ?>', $resultContent);
-        $resultContent = preg_replace('/^#else$/m', '<?php else: ?>', $resultContent);
-        $resultContent = preg_replace('/^#elif\s+(.+)/m', '<?php elseif ($1): ?>', $resultContent);
-        $resultContent = preg_replace('/^#endif$/m', '<?php endif; ?>', $resultContent);
+        $resultContent = preg_replace('/^#if\s+(.+)$/mUS', '<?php if ($1): ?>', $resultContent);
+        $resultContent = preg_replace('/^#else$/mUS', '<?php else: ?>', $resultContent);
+        $resultContent = preg_replace('/^#elif\s+(.+)/mUS', '<?php elseif ($1): ?>', $resultContent);
+        $resultContent = preg_replace('/^#endif$/mUS', '<?php endif; ?>', $resultContent);
 
         return $resultContent;
     }
--- src/Parser/PhpTagParser.php
+++ src/Parser/PhpTagParser.php
@@ -15,8 +15,8 @@ class PhpTagParser implements IMacroParser
     public function parse(string $content): string
     {
         $resultContent = $content;
-        $resultContent = preg_replace('/<\?php([\s]*?)([\r\n]*)/', sprintf('%s echo \'%s\'; %s$1$2$2', self::PHP_TAG_BEGIN, self::PHP_TAG_BEGIN, self::PHP_TAG_END), $resultContent);
-        $resultContent = preg_replace('/\?>/', sprintf('%s echo \'%s\'; %s', self::PHP_TAG_BEGIN, self::PHP_TAG_END, self::PHP_TAG_END), $resultContent);
+        $resultContent = preg_replace('/<\?php(\s*?)([\r\n]*)/S', sprintf('%s echo \'%s\'; %s$1$2$2', self::PHP_TAG_BEGIN, self::PHP_TAG_BEGIN, self::PHP_TAG_END), $resultContent);
+        $resultContent = preg_replace('/\?>/S', sprintf('%s echo \'%s\'; %s', self::PHP_TAG_BEGIN, self::PHP_TAG_END, self::PHP_TAG_END), $resultContent);
         $resultContent = str_replace([
             self::PHP_TAG_BEGIN,
             self::PHP_TAG_END,
